d4295aa452e5e1cccef20c616fe6303993c78ca0
Daniel Osorio <cristian.osorio@doublevpartners.com>
2024-02-01 17:46:12 -0500

    feat: initial folder structure and app layout initiated

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GithubApp extends StatelessWidget {
+  const GithubApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:githun_api_commits/app.dart';
+
+void main() {
+  runApp(const GithubApp());
+}
