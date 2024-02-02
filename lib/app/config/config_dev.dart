e49e1f4b5fdd7cde2772ec3e1c7f7680a294baf7
Daniel Osorio <tiancris01@gmail.com>
2024-02-01 19:33:06 -0500

    feat: baseconfig env implemented

---
+++ lib/app/config/config_dev.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:githun_api_commits/app/types/base_config.dart';
+
+class ConfigDev extends BaseConfig {
+  @override
+  String get environment => dotenv.env['ENVIRONMENT'] ?? '';
+
+  @override
+  bool get bannerVisibility =>
+      dotenv.env['BANNER_VISIBILITY']?.contains("true") == true;
+
+  @override
+  String get apiBaseUrl => dotenv.env['API_BASE_URL'] ?? '';
+}
