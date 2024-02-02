2ff0a437bc4cea21ac6af7a1c8aeda7c0b2ec3d4
Daniel Osorio <tiancris01@gmail.com>
2024-02-02 09:20:54 -0500

    refactor: folder structure modified and files moved

--- lib/features/github/commits/domain/github_failure.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'github_failure.freezed.dart';
-
-@freezed
-class GithubFailure with _$GithubFailure {
-  const GithubFailure._();
-  const factory GithubFailure.api(int? errorCode) = _Api;
-}
